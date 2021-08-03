04633f73aeabe7a0578d7db0b7559ca625dc0fa4
TurtleArmy <44322335+TurtleArmyMc@users.noreply.github.com>
2021-08-03 19:15:53 -0400

    Make warn command create warnings_path on save.

--- commands/warn.py
+++ commands/warn.py
@@ -8,23 +8,24 @@ from cmd import Bot_Command, bot_commands
 from pathlib import Path
 from datetime import datetime, time, timedelta
 from utils import get_member
-#from commands.mute import command as mute
+
+# from commands.mute import command as mute
+
 
 class Warn_Command(Bot_Command):
     name = "warn"
 
     short_help = "Warn a member in the server.\n**$warn [username/user id/nickname] [Optional reason]**\nGet the count of warns on a user.\n**$warn count [username/user id/nickname]**"
 
-    long_help = """~Warns a member in the server or gets the count of warns on a user. 
-    Warn Command Syntax: 
+    long_help = """~Warns a member in the server or gets the count of warns on a user.
+    Warn Command Syntax:
     **$warn [member] [Optional reason]**
     **$warn count [member]**
     `member`: *@User, User ID, Nickname* (Not Case Sensitive)
     'Optional reason': *Text* (Can have spaces)
 
-    ~Example: (Assume this is the same user) 
+    ~Example: (Assume this is the same user)
     **$warn TheLegend47** or **$warn [1234567891012345678]** or **$warn Legend** """
-
 
     def __init__(self):
         self.warnings_path = Path("data/warn/warnings.json")
@@ -37,9 +38,11 @@ class Warn_Command(Bot_Command):
                 self.warning = json.load(file)
 
     def save_warnings(self):
+        self.warnings_path.parent.mkdir(parents=True, exist_ok=True)
         with self.warnings_path.open("w") as file:
             json.dump(self.warning, file, indent=3)
-#TODO command name "count"
+
+    # TODO command name "count"
     async def run(self, msg: discord.Message, args: str):
         if args.casefold().startswith("count ") and len(args) > len("count "):
             split_args = args.split(" ", 1)
@@ -49,18 +52,26 @@ class Warn_Command(Bot_Command):
             user_name = split_args[1]
             member = await get_member(msg.channel, user_name, msg.author)
             if member is None:
-                await msg.channel.send(f"Error: **{user_name}** not found. If you are having trouble typing in a user's name, you can also use their User ID! Example: **$warn count {random.randint(100000000000000000, 999999999999999999)}**")
+                await msg.channel.send(
+                    f"Error: **{user_name}** not found. If you are having trouble typing in a user's name, you can also use their User ID! Example: **$warn count {random.randint(100000000000000000, 999999999999999999)}**"
+                )
                 return
             user_id = str(member.id)
             guild_id = str(msg.guild.id)
             if guild_id not in self.warning:
-                await msg.channel.send("Error: No one in this server has been warned yet!")
+                await msg.channel.send(
+                    "Error: No one in this server has been warned yet!"
+                )
                 return
             if user_id not in self.warning[guild_id]:
-                await msg.channel.send("This user has not been warned yet on this server.")
+                await msg.channel.send(
+                    "This user has not been warned yet on this server."
+                )
                 return
             if user_id in self.warning[guild_id]:
-                await msg.channel.send(f"**{member}** has been warned {self.warning[guild_id][user_id]['warning_count']} time(s).\nLast time warned: **{self.warning[guild_id][user_id]['last_warned_at']}** by **{self.warning[guild_id][user_id]['last_warned_by']}** ")
+                await msg.channel.send(
+                    f"**{member}** has been warned {self.warning[guild_id][user_id]['warning_count']} time(s).\nLast time warned: **{self.warning[guild_id][user_id]['last_warned_at']}** by **{self.warning[guild_id][user_id]['last_warned_by']}** "
+                )
             return
 
         if msg.author.guild_permissions.administrator:
@@ -70,16 +81,16 @@ class Warn_Command(Bot_Command):
             split_args = args.split(" ", 1)
             """ split_args[0] = user_id
                 split_args[1] = reason   """
-            
-            
+
             user_name = split_args[0]
             member = await get_member(msg.channel, user_name, msg.author)
             if member is None:
-                await msg.channel.send(f"Error: **{user_name}** not found. If you are having trouble typing in a user's name, you can also use their User ID! Example: **$warn {random.randint(100000000000000000, 999999999999999999)}**")
+                await msg.channel.send(
+                    f"Error: **{user_name}** not found. If you are having trouble typing in a user's name, you can also use their User ID! Example: **$warn {random.randint(100000000000000000, 999999999999999999)}**"
+                )
                 return
             user_id = str(member.id)
 
-            
             guild_id = str(msg.guild.id)
             if guild_id not in self.warning:
                 self.warning[guild_id] = {}
@@ -117,12 +128,12 @@ class Warn_Command(Bot_Command):
                 self.warning[guild_id][user_id]["reasons"].append(reason)
                 self.warning[guild_id][user_id]["message_logs"].append(message_logs)
                 self.save_warnings()
-                                
+
                 warning_message = discord.Embed(
-                    title = "You have been warned!",
-                    description = f"~You have been warned by **{msg.author}** from the server: **{msg.guild.name}**\n\n~The reason you were warned: **{reason}**\n\n~Since this is your first warning, nothing will happen to you. However, if you keep getting warned by mods you will either get muted or possibly banned depending on how many times you have been warned.\n\n~Be sure to not spam chats, say anything that would offend someone else, or post NSFW pictures in chats unless they are labeled NSFW.\n\n~If you believe you have been accidentally or wrongfully warned, don't hesitate to ping or PM a mod so that they can look into it.",
-                    color = red
-                    )
+                    title="You have been warned!",
+                    description=f"~You have been warned by **{msg.author}** from the server: **{msg.guild.name}**\n\n~The reason you were warned: **{reason}**\n\n~Since this is your first warning, nothing will happen to you. However, if you keep getting warned by mods you will either get muted or possibly banned depending on how many times you have been warned.\n\n~Be sure to not spam chats, say anything that would offend someone else, or post NSFW pictures in chats unless they are labeled NSFW.\n\n~If you believe you have been accidentally or wrongfully warned, don't hesitate to ping or PM a mod so that they can look into it.",
+                    color=red,
+                )
                 await member.send(embed=warning_message)
                 await msg.channel.send(
                     f"**{member}** has been warned. They have been warned **{self.warning[guild_id][user_id]['warning_count']}** time(s)."
@@ -131,50 +142,74 @@ class Warn_Command(Bot_Command):
 
             if user_id in self.warning[guild_id]:
                 self.warning[guild_id][user_id]["warning_count"] += 1
-                self.warning[guild_id][user_id]["last_warned_at"] = user_info["last_warned_at"]
-                self.warning[guild_id][user_id]["last_warned_by"] = user_info["last_warned_by"]
+                self.warning[guild_id][user_id]["last_warned_at"] = user_info[
+                    "last_warned_at"
+                ]
+                self.warning[guild_id][user_id]["last_warned_by"] = user_info[
+                    "last_warned_by"
+                ]
                 self.warning[guild_id][user_id]["reasons"].append(reason)
                 self.warning[guild_id][user_id]["message_logs"].append(message_logs)
                 self.save_warnings()
-                
+
                 previous_reasons = ""
                 for reason in self.warning[guild_id][user_id]["reasons"]:
                     previous_reasons += "\n" + reason
 
-                #https://stackoverflow.com/questions/9647202/ordinal-numbers-replacement  
-                ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4])
+                # https://stackoverflow.com/questions/9647202/ordinal-numbers-replacement
+                ordinal = lambda n: "%d%s" % (
+                    n,
+                    "tsnrhtdd"[(n // 10 % 10 != 1) * (n % 10 < 4) * n % 10 :: 4],
+                )
                 warning_count = self.warning[guild_id][user_id]["warning_count"]
 
                 if warning_count == 1:
                     warning_message = discord.Embed(
-                        title = "You have been warned!",
-                        description = f"~You have been warned by **{msg.author}** from the server: **{msg.guild.name}**\n\n~The reason you were warned: **{reason}**\n\n~This is your **{ordinal(warning_count)}** warning.\n\n~Previous Reason(s): **{previous_reasons}**\n\n~These are the punishments for a warning beyond the first one:\nTwo Warnings: Server Muted for **6 Hours**\nThree Warnings: Server Muted for **3 Days + SlowMode (One message per minute)**\nFour+ Warnings: Server Muted for 7 days + A moderator will deal with you manually. This can result in a **permanent ban** or **permanent mute**.\n\n~Be sure to not spam chats, say anything that would offend someone else, or post NSFW pictures in chats unless they are labeled NSFW.\n\n~If you believe you have been accidentally or wrongfully warned, don't hesitate to ping or PM a mod so that they can look into it.",
-                        color = red#TODO color 
+                        title="You have been warned!",
+                        description=f"~You have been warned by **{msg.author}** from the server: **{msg.guild.name}**\n\n~The reason you were warned: **{reason}**\n\n~This is your **{ordinal(warning_count)}** warning.\n\n~Previous Reason(s): **{previous_reasons}**\n\n~These are the punishments for a warning beyond the first one:\nTwo Warnings: Server Muted for **6 Hours**\nThree Warnings: Server Muted for **3 Days + SlowMode (One message per minute)**\nFour+ Warnings: Server Muted for 7 days + A moderator will deal with you manually. This can result in a **permanent ban** or **permanent mute**.\n\n~Be sure to not spam chats, say anything that would offend someone else, or post NSFW pictures in chats unless they are labeled NSFW.\n\n~If you believe you have been accidentally or wrongfully warned, don't hesitate to ping or PM a mod so that they can look into it.",
+                        color=red,  # TODO color
                     )
                     await member.send(embed=warning_message)
                     await msg.channel.send(
-                    f"**{member}** has been warned. They have been warned **{self.warning[guild_id][user_id]['warning_count']}** time(s)."
+                        f"**{member}** has been warned. They have been warned **{self.warning[guild_id][user_id]['warning_count']}** time(s)."
                     )
                     return
                 elif warning_count == 2:
-                    await mute.mute(m = member, unmute_at = datetime.now() + timedelta(hours = 6), channel = msg.channel, author = msg.author)
+                    await mute.mute(
+                        m=member,
+                        unmute_at=datetime.now() + timedelta(hours=6),
+                        channel=msg.channel,
+                        author=msg.author,
+                    )
                 elif warning_count == 3:
-                    await mute.mute(m = member, unmute_at = datetime.now() + timedelta(days = 3), channel = msg.channel, author = msg.author)
-                elif warning_count > 3:#TODO welcome back, you're still muted
-                    await mute.mute(m = member, unmute_at = datetime.now() + timedelta(days = 7), channel = msg.channel, author = msg.author)
-                    await msg.channel.send("**Notice:** This user has been warned over 3 times on this server.")
+                    await mute.mute(
+                        m=member,
+                        unmute_at=datetime.now() + timedelta(days=3),
+                        channel=msg.channel,
+                        author=msg.author,
+                    )
+                elif warning_count > 3:  # TODO welcome back, you're still muted
+                    await mute.mute(
+                        m=member,
+                        unmute_at=datetime.now() + timedelta(days=7),
+                        channel=msg.channel,
+                        author=msg.author,
+                    )
+                    await msg.channel.send(
+                        "**Notice:** This user has been warned over 3 times on this server."
+                    )
                 warning_message = discord.Embed(
-                    title = "You have been warned!",
-                    description = f"~You have been warned by **{msg.author}** from the server: **{msg.guild.name}**\n\n~The reason you were warned: **{reason}**\n\n~This is your **{ordinal(warning_count)}** warning.\n\n~Previous Reason(s): **{previous_reasons}**\n\n~These are the punishments for a warning beyond the first one:\nTwo Warnings: Server Muted for **6 Hours**\nThree Warnings: Server Muted for **3 Days + SlowMode (One message per minute)**\nFour+ Warnings: Server Muted for 7 days + A moderator will deal with you manually. This can result in a **permanent ban** or **permanent mute**.\n\n~Be sure to not spam chats, say anything that would offend someone else, or post NSFW pictures in chats unless they are labeled NSFW.\n\n~If you believe you have been accidentally or wrongfully warned, don't hesitate to ping or PM a mod so that they can look into it.",
-                    color = red#TODO color 
+                    title="You have been warned!",
+                    description=f"~You have been warned by **{msg.author}** from the server: **{msg.guild.name}**\n\n~The reason you were warned: **{reason}**\n\n~This is your **{ordinal(warning_count)}** warning.\n\n~Previous Reason(s): **{previous_reasons}**\n\n~These are the punishments for a warning beyond the first one:\nTwo Warnings: Server Muted for **6 Hours**\nThree Warnings: Server Muted for **3 Days + SlowMode (One message per minute)**\nFour+ Warnings: Server Muted for 7 days + A moderator will deal with you manually. This can result in a **permanent ban** or **permanent mute**.\n\n~Be sure to not spam chats, say anything that would offend someone else, or post NSFW pictures in chats unless they are labeled NSFW.\n\n~If you believe you have been accidentally or wrongfully warned, don't hesitate to ping or PM a mod so that they can look into it.",
+                    color=red,  # TODO color
                 )
                 await member.send(embed=warning_message)
                 await msg.channel.send(
-                f"**{member}** has been warned. They have been warned **{self.warning[guild_id][user_id]['warning_count']}** time(s)."
+                    f"**{member}** has been warned. They have been warned **{self.warning[guild_id][user_id]['warning_count']}** time(s)."
                 )
                 return
-        
         else:
+            # TODO: Rewrite error message.
             await msg.channel.send("You need to be admin lol")
 
 
