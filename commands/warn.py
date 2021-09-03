e950709eacfd4a72f9efd7fc2385c00c9be22411
lempedrado <39011441+lempedrado@users.noreply.github.com>
2021-09-02 22:22:25 -0400

    Converted warn.py from JSON to mySQL database

--- commands/warn.py
+++ commands/warn.py
@@ -1,17 +1,13 @@
 from logging import warn
-import discord
-import json
-import random
 from commands.mute import mute
-from commands.unmute import unmute
 from bot_cmd import Bot_Command, bot_commands, Bot_Command_Category
-from pathlib import Path
-from datetime import datetime, time, timedelta
+from datetime import datetime, timedelta
 from utils import find, fmt, std_embed
 from utils.errors import ReportableError
 
-
-# from commands.mute import command as mute
+import discord
+import random
+import db
 
 
 class Warn_Command(Bot_Command):
@@ -19,7 +15,7 @@ class Warn_Command(Bot_Command):
 
     short_help = "Warn a member in the server.\n**$warn [username/user id/nickname] [Optional reason]**\nGet the count of warns on a user.\n**$warn count [username/user id/nickname]**"
 
-    long_help = """~Warns a member in the server or gets the count of warns on a user.
+    long_help = """Warns a member in the server or gets the count of warns on a user.
     Warn Command Syntax:
     **$warn [member] [Optional reason]**
     **$warn count [member]**
@@ -33,25 +29,22 @@ class Warn_Command(Bot_Command):
     category = Bot_Command_Category.MODERATION
 
     def __init__(self):
-        self.warnings_path = Path("data/warn/warnings.json")
-        if not self.warnings_path.exists():
+        db.execute("""CREATE TABLE IF NOT EXISTS warn (
+                Server bigint,
+                Member bigint,
+                Count int,
+                DT datetime,
+                Moderator text,
+                Reason text,
+                MsgLog text,
+                PRIMARY KEY (Server, Member, Count)
+            );"""
+        )
 
-            self.warning = {}
-            self.save_warnings()
-        else:
-            with self.warnings_path.open() as file:
-                self.warning = json.load(file)
-
-    def save_warnings(self):
-        self.warnings_path.parent.mkdir(parents=True, exist_ok=True)
-        with self.warnings_path.open("w") as file:
-            json.dump(self.warning, file, indent=3)
 
     # TODO command name "count"
     async def run(self, msg: discord.Message, args: str):
-        if args.casefold().startswith("count ") and len(args) > len("count "):
-            if not args:
-                raise ReportableError("No user entered")
+        if args.casefold().startswith("count ") and args[len("count "):]:
             split_args = args.split(" ", 1)
             print(split_args)
             """ split_args[0] = count
@@ -66,38 +59,54 @@ class Warn_Command(Bot_Command):
                         random.randint(100000000000000000, 999999999999999999),
                     )
                 )
-            user_id = str(member.id)
-            guild_id = str(msg.guild.id)
-            if guild_id not in self.warning:
-                raise ReportableError("No one in this server has been warned yet!")
-            if user_id not in self.warning[guild_id]:
+            #get the member's most recent warn
+            operation = "SELECT Count, DT, Moderator FROM warn WHERE Count = (SELECT MAX(Count) FROM warn WHERE Server = %s AND Member = %s);"
+            params = (msg.guild.id, member.id)
+            result = db.read_execute(operation, params)
+            if not result:
                 await std_embed.send_info(
                     msg.channel,
                     title="This user has not been warned yet on this server.",
                     author=msg.author,
                 )
-                return
-            if user_id in self.warning[guild_id]:
-                fmt.format_maxlen(
-                    "**{}** has been warned {} "
-                    f"{'time' if self.warning[guild_id][user_id]['warning_count'] == 1 else 'times'}."
-                    "\nLast time warned: **{}** by **{}**",
-                    member,
-                    self.warning[guild_id][user_id]["warning_count"],
-                    self.warning[guild_id][user_id]["last_warned_at"],
-                    self.warning[guild_id][user_id]["last_warned_by"],
+            else:
+                await std_embed.send_info(
+                    msg.channel,
+                    description=fmt.format_maxlen(
+                        "**{}** has been warned {} "
+                        f"{'time' if result[0][0] == 1 else 'times'}."
+                        "\nLast warned: **<t:{}>** by **{}**",
+                        member,
+                        result[0][0],
+                        int(result[0][1].timestamp()),
+                        result[0][2],
+                    ),
+                    author=member
                 )
-            return
 
-        if msg.author.guild_permissions.administrator:
+        elif msg.author.guild_permissions.administrator:
+            #if a member is not provided to warn
             if not args:
                 raise ReportableError("No user entered")
-            split_args = args.split(" ", 1)
+            split_args = args.split(",", 1)
+
+            #check if a reason for the warn is provided
+            if len(split_args) < 2:
+                user_name = args
+                #TODO check if an attachment was provided as the reason
+                #if msg.attachments:
+                #    pass
+                #else:
+                #    pass
+                reason = "No reason given"
+            else:
+                user_name = split_args[0]
+                reason = split_args[1]
             """ split_args[0] = user_id
                 split_args[1] = reason   """
 
-            user_name = split_args[0]
             member = await find.member(msg.channel, user_name, msg.author)
+            #if the member specified cannot be found
             if member is None:
                 raise ReportableError(
                     fmt.format_maxlen(
@@ -106,26 +115,10 @@ class Warn_Command(Bot_Command):
                         random.randint(100000000000000000, 999999999999999999),
                     )
                 )
-            user_id = str(member.id)
-            guild_id = str(msg.guild.id)
-            if guild_id not in self.warning:
-                self.warning[guild_id] = {}
-                self.save_warnings()
+            elif member.bot:
+                raise ReportableError("You cannot warn bots.")
+
             red = 0xFF0000  # red
-            warner = str(msg.author)
-            user_info = {
-                "warning_count": 1,
-                "last_warned_at": str(datetime.now().replace(microsecond=0)),
-                "last_warned_by": warner,
-                "reasons": [],
-                "message_logs": [],
-            }
-
-            if len(split_args) < 2:
-                reason = "No reason given"
-            else:
-                reason = split_args[1]
-
             # run this 5 times
             check_counter = 0
             message_logs = []
@@ -134,57 +127,81 @@ class Warn_Command(Bot_Command):
                     break
                 if m.author == member:
                     check_counter += 1
-                    message_logs.append(m.content)
+                    message_logs.insert(0, m.content)
 
-            message_logs.reverse()
-            punishments = "~These are the punishments for a warning beyond the first one:\nTwo Warnings: Server Muted for **6 Hours**\nThree Warnings: Server Muted for **3 Days**\nFour+ Warnings: Server Muted for **7 days** + A moderator will deal with you manually. This can result in a **permanent ban** or **permanent mute**."
-            if user_id not in self.warning[guild_id]:
-                self.warning[guild_id][user_id] = {}
-                self.warning[guild_id][user_id].update(user_info)
-                self.warning[guild_id][user_id]["reasons"].append(reason)
-                self.warning[guild_id][user_id]["message_logs"].append(message_logs)
-                self.save_warnings()
+            punishments = """~These are the punishments for a warning beyond the first one:
+                Two Warnings: Server Muted for **6 Hours**
+                Three Warnings: Server Muted for **3 Days**
+                Four+ Warnings: Server Muted for **7 days** + A moderator will deal with you manually. This can result in a **permanent ban** or **permanent mute**.
+                """
 
+            #try to get the members previous warnings
+            operation = "SELECT * FROM warn WHERE Server = %s AND Member = %s ORDER BY Count;"
+            params = (msg.guild.id, member.id)
+            warnings = db.read_execute(operation, params)
+
+            #the user's first warning
+            if not warnings:
+                #log the warn in the table
+                operation = "INSERT INTO warn VALUES (%s, %s, %s, %s, %s, %s, %s);"
+                params = (msg.guild.id, member.id, 1, datetime.now().replace(microsecond=0), str(msg.author), reason, "\\n".join(message_logs))
+                db.execute(operation, params)
+                #send the user a private message explaining the warn
                 warning_message = discord.Embed(
                     title="You have been warned!",
-                    description=f"~You have been warned by **{msg.author}** from the server: **{msg.guild.name}**\n\n~The reason you were warned: **{reason}**\n\n~Since this is your first warning, nothing will happen to you. However, if you keep getting warned by mods you will either get muted or possibly banned depending on how many times you have been warned.\n\n{punishments}\n\n~Be sure to not spam chats, say anything that would offend someone else, or post NSFW pictures in chats unless they are labeled NSFW.\n\n~If you believe you have been accidentally or wrongfully warned, don't hesitate to ping or PM a mod so that they can look into it.",
+                    description=f"""
+                        ~You have been warned by **{msg.author}** from the server: **{msg.guild.name}**\n
+                        ~The reason you were warned: **{reason}**\n
+                        ~Since this is your first warning, nothing will happen to you. However, future warnings may result in a mute or possible ban.\n
+                        {punishments}\n
+                        ~Be sure to not spam chats, say anything that would offend someone else, or post NSFW pictures in chats unless they are labeled NSFW.\n
+                        ~If you believe you have been accidentally or wrongfully warned, don't hesitate to ping or PM a mod so that they can look into it.
+                        """,
                     color=red,
                 )
                 await member.send(embed=warning_message)
-                await std_embed.send_info(msg.channel, 
+                await std_embed.send_info(msg.channel,
                     title = f"{member} has been warned. This is their first warning.",
                     author = msg.author
                 )
-                return
 
-            if user_id in self.warning[guild_id]:
-                self.warning[guild_id][user_id]["warning_count"] += 1
-                self.warning[guild_id][user_id]["last_warned_at"] = user_info[
-                    "last_warned_at"
-                ]
-                self.warning[guild_id][user_id]["last_warned_by"] = user_info[
-                    "last_warned_by"
-                ]
-                self.warning[guild_id][user_id]["reasons"].append(reason)
-                self.warning[guild_id][user_id]["message_logs"].append(message_logs)
-                self.save_warnings()
+            #the user's successive warning
+            else:
+                #get how many times this member has been muted
+                operation = "SELECT MAX(Count) FROM  warn WHERE Server = %s AND Member = %s;"
+                warning_count = db.read_execute(operation, params)[0][0] + 1
 
-                previous_reasons = ""
-                for reason in self.warning[guild_id][user_id]["reasons"]:
-                    previous_reasons += "\n" + reason
+                #log the warning
+                operation = "INSERT INTO warn VALUES (%s, %s, %s, %s, %s, %s, %s);"
+                params = (msg.guild.id, member.id, warning_count, datetime.now().replace(microsecond=0), str(msg.author), reason, "\\n".join(message_logs))
+                db.execute(operation, params)
+
+                operation = "SELECT Reason FROM warn WHERE Server = %s AND Member = %s ORDER BY Count;"
+                params = (msg.guild.id, member.id)
+                previous_reasons = db.read_execute(operation, params)
+                previous_reasons = "\n".join(reason[0] for reason in previous_reasons)
 
                 # https://stackoverflow.com/questions/9647202/ordinal-numbers-replacement
                 ordinal = lambda n: "%d%s" % (
                     n,
                     "tsnrhtdd"[(n // 10 % 10 != 1) * (n % 10 < 4) * n % 10 :: 4],
                 )
-                warning_count = self.warning[guild_id][user_id]["warning_count"]
 
                 warning_message = discord.Embed(
-                title="You have been warned!",
-                description=f"~You have been warned by **{msg.author}** from the server: **{msg.guild.name}**\n\n~The reason you were warned: **{reason}**\n\n~This is your **{ordinal(warning_count)}** warning.\n\n~Previous Reason(s): **{previous_reasons}**\n\n{punishments}\n\n~Be sure to not spam chats, say anything that would offend someone else, or post NSFW pictures in chats unless they are labeled NSFW.\n\n~If you believe you have been accidentally or wrongfully warned, don't hesitate to ping or PM a mod so that they can look into it.",
-                color=red,  # TODO color
+                    title="You have been warned!",
+                    description=f"""
+                        ~You have been warned by **{msg.author}** from the server: **{msg.guild.name}**\n
+                        ~The reason you were warned: **{reason}**\n
+                        ~This is your **{ordinal(warning_count)}** warning.\n
+                        ~Previous Reason(s): \n**{previous_reasons}**\n
+                        {punishments}
+                        ~Be sure to not spam chats, say anything that would offend someone else, or post NSFW pictures in chats unless they are labeled NSFW.\n
+                        ~If you believe you have been accidentally or wrongfully warned, don't hesitate to ping or PM a mod so that they can look into it.
+                        """,
+                    color=red,  # TODO color
                 )
+
+                #handle the member appropriately depending on how many times they've been warned
                 if warning_count == 2:
                     await member.send(embed=warning_message)
                     await std_embed.send_info(msg.channel,
@@ -197,11 +214,10 @@ class Warn_Command(Bot_Command):
                         channel=msg.channel,
                         author=msg.author,
                     )
-                    return
                 elif warning_count == 3:
                     await member.send(embed=warning_message)
                     await std_embed.send_info(msg.channel,
-                        title = f"{member} has been warned. This is their {ordinal(warning_count)} warning.", 
+                        title = f"{member} has been warned. This is their {ordinal(warning_count)} warning.",
                         author = msg.author
                     )
                     await mute.mute(
@@ -210,7 +226,6 @@ class Warn_Command(Bot_Command):
                         channel=msg.channel,
                         author=msg.author,
                     )
-                    return
                 elif warning_count > 3:  # TODO welcome back, you're still muted
                     await member.send(embed=warning_message)
                     await std_embed.send_info(msg.channel,
@@ -226,6 +241,6 @@ class Warn_Command(Bot_Command):
                         channel=msg.channel,
                         author=msg.author,
                     )
-                    return
+
 
 bot_commands.add_command(Warn_Command())
